c74f1628c6c4e8bc7cacf1c5a219e2c38482ab36
EvgeniyaMarchenkova <evgeniova@gmail.com>
2017-08-29 16:14:04 +0300

    Task-2: removing contact added

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -2,9 +2,16 @@ var app = angular.module('myApp');
 
 app.controller('MainController', function ($scope, contactListService) {
     $scope.shownContacts = contactListService.getAll();
+    $scope.isPhoneShown = true;
+
     $scope.updateList = function () {
         $scope.shownContacts = contactListService.getContact($scope.substrFilter);
     }
-    $scope.isPhoneShown = true;
+
+    $scope.removeContact = function(name) {
+        contactListService.removeContact(name);
+        $scope.shownContacts = contactListService.getAll();
+        $scope.substrFilter = '';
+    }
     
 })
--- services/contactListService.js
+++ services/contactListService.js
@@ -1,6 +1,7 @@
 var app = angular.module('myApp');
 
 app.service('contactListService', function(filterFilter) {
+
     var contacts = [
         {name:'Pol Smith', phone:'1231212'},
         {name:'Jessika Smith', phone:'1235555'},
@@ -9,16 +10,23 @@ app.service('contactListService', function(filterFilter) {
         {name:'avid White', phone:'1232133'},
         {name:'bavid White', phone:'1232133'},
     ];
+
     this.getAll = function () {
         return contacts;
     }
+
     this.getContact = function(str) {
         return filterFilter(contacts, str)
     }
+
     this.addContact = function(name, phone) {
         contacts.push({'name':name, 'phone':phone})
     }
-    this.removeContact = function(name, phone) {
-        contacts.push({'name':name, 'phone':phone})
+
+    this.removeContact = function(name) {
+        contacts = contacts.filter(function (contact, i) {
+            return contact.name !== name;
+        })
     }
+
 });
