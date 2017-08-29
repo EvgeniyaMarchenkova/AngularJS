f08450e8e909faf2d9fb0fd338ace995825dccef
Evgeniya Marchenkova <evgeniova@gmail.com>
2017-08-29 08:37:10 +0300

    Task-2:filter added

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,14 +1,12 @@
 var app = angular.module('myApp');
 
-app.controller('MainController', function ($scope) {
-    $scope.contacts = [
-        {name:'Pol Smith', phone:'1231212'},
-        {name:'Jessika Smith', phone:'1235555'},
-        {name:'Crys Black', phone:'1239988'},
-        {name:'David White', phone:'1232133'},
-        {name:'avid White', phone:'1232133'},
-        {name:'bavid White', phone:'1232133'},
-    ];
+app.controller('MainController', function ($scope, contactListService) {
+    $scope.substrFilter = '';
+    if ($scope.substrFilter ) {
+
+    }
+    $scope.shownContacts = contactListService.getContact($scope.substrFilter);
+
     $scope.isPhoneShown = true;
     
 })
---
+++ services/contactListService.js
@@ -0,0 +1,22 @@
+var app = angular.module('myApp');
+
+app.service('contactListService', function(filterFilter) {
+    var contacts = [
+        {name:'Pol Smith', phone:'1231212'},
+        {name:'Jessika Smith', phone:'1235555'},
+        {name:'Crys Black', phone:'1239988'},
+        {name:'David White', phone:'1232133'},
+        {name:'avid White', phone:'1232133'},
+        {name:'bavid White', phone:'1232133'},
+    ];
+    this.getAll = function () {
+        return contacts;
+    }
+    this.getContact = function(str) {
+        console.log(str)
+        return filterFilter(contacts, str)
+    }
+    this.addContact = function(name, phone) {
+        contacts.push({'name':name, 'phone':phone})
+    }
+});
