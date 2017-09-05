f95294e1c440662363508351337cd4f2ed57bed1
EvgeniyaMarchenkova <evgeniova@gmail.com>
2017-09-05 12:36:32 +0300

    router implemented

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-angular.module('myApp', []).run(function(contactListService) {
-    var startContacts = [
-        {name:'Pol Smith', phone:'1231212'},
-        {name:'Jessika Smith', phone:'1235555'},
-        {name:'Crys Black', phone:'1239988'},
-        {name:'David White', phone:'1232133'},
-        {name:'avid White', phone:'1232133'},
-        {name:'bavid White', phone:'1232133'},
-    ];
+angular.module('myApp', ['ui.router'])
+    .run(function(contactListService, $state) {
+        var startContacts = [
+            {name:'Pol Smith', phone:'1231212'},
+            {name:'Jessika Smith', phone:'1235555'},
+            {name:'Crys Black', phone:'1239988'},
+            {name:'David White', phone:'1232133'},
+            {name:'Androu White', phone:'1232133'},
+            {name:'Bill White', phone:'1232133'},
+        ];
+    
+        contactListService.updateStorage(startContacts);
+        $state.go('contactsList');
 
-    contactListService.updateStorage(startContacts);
-});
+    })
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('contactsList', {
+                url: '/',
+                templateUrl:  './templates/phoneBookTemplate.html'
+            })
+            .state('contactForm', {
+                url: '/contactForm',
+                templateUrl:  './templates/contactFormTemplate.html'
+            });
+    })
